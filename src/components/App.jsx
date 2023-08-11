6a95f846ff6a320fab47afe8a7f2fcc10fb30a1a
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-08-11 20:32:38 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { GlobalStyle, Container } from './GlobalStyle';
+import React, { Component } from 'react';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  // handleLeaveFeedback = e => {
+  //   this.setState(prevState => {
+  //     return {
+  //       [e]: prevState[e] + 1,
+  //     };
+  //   });
+  // };
+
+  // totalFeedback = ({ good, neutral, bad }) => good + neutral + bad;
+
+  render() {
+    // const { good, neutral, bad } = this.state;
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm />
+
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,4 @@
+// import { nanoid } from 'nanoid';
+// import { Formik } from 'formik';
+
+export const ContactForm = () => {};
